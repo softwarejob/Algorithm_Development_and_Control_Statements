ee683db8b398fd35204c1e68268e8791ea16abc2
shao-chelin <a4061476@gmail.com>
2024-07-02 16:50:14 +0800

    update

---
+++ if...else Double-Selection Statement.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Shao-Che Lin on 2024/7/2.
+//
+//Pseudocode:
+/*if student's grade is greater than or equal to 60
+print "Passed" （第二行縮排為optional，為清晰起見，建議使用）
+else
+print "Failed"
+
+This pseudocode may be written in C++ as:
+ */
+
+if (studentGrade >= 60 {
+cout << "Passed";
+}
+else {
+cout << "Failed";
+
+}
